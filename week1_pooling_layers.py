9f87c20ded4596ea33acc06bdac5a3ea0cc76b91
Felix Flory <felix.flory@gmail.com>
2022-06-29 13:42:35 -0500

    deleted individual python files

--- week1_pooling_layers.py
+++
@@ -1,9 +0,0 @@
-pooling is easy
-hyperparameters are:
-f = filter size (common value: 2)
-s = stride size (common value: 2)
-
-no parameters to learn!
-
-# formula also works for pooling
-int(math.floor(((n + 2*p - f) / s) + 1))
